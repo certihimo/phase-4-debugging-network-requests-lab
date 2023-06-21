0eb0ba3da180c191e4a86766592d49a67592420a
certihimo <kalidahm3d@gmail.com>
2023-06-21 14:25:29 +0300

    done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,22 +7,27 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
     toy = Toy.find_by(id: params[:id])
     toy.destroy
     head :no_content
+    render json: toy
   end
 
   private
+  def render_not_found_response
+    render json: {error: "Toy not found"}, status: :not_found
+  end
   
   def toy_params
     params.permit(:name, :image, :likes)
